c1cd90601108a613114e034748c9bb6406e6918c
christian.weber <Christian-Weber1@gmx.at>
2015-01-07 17:12:39 +0100

    DefaultExecutionUserService moved to module arrow-service

--- arrow-service/src/main/java/org/arrow/service/DefaultExecutionUserService.java
+++ arrow-service/src/main/java/org/arrow/service/DefaultExecutionUserService.java
@@ -1,23 +1,20 @@
 /*
  * Copyright 2014 Christian Weber
  *
- * This file is build on Neo4j.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * Neo4j is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
-package org.arrow.data;
+package org.arrow.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -29,14 +26,14 @@ import org.arrow.runtime.message.EventMessage;
 import org.arrow.runtime.message.impl.DefaultFinishEventMessage;
 
 /**
- * Neo4j {@link ExecutionUserService} implementation.
+ * Default {@link ExecutionUserService} implementation.
  *
  * @since 1.0.0
  * @author christian.weber
  */
 @Service
 @Transactional
-public class Neo4jExecutionUserService implements ExecutionUserService {
+public class DefaultExecutionUserService implements ExecutionUserService {
 
     @Autowired
     private RuntimeService runtimeService;
